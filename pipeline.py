f33b0db80640564c6ac1d14bb67b2c29413a07f0
Super Bo <supernbo@gmail.com>
2021-12-27 11:55:03 +0700

    Change pipeline config

--- pipeline.py
+++ pipeline.py
@@ -10,16 +10,26 @@ class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        pipeline =  CodePipeline(self, 'Pipeline', 
+        github_arn = self.format_arn(
+            region='us-east-1',
+            service='codestar-connections',
+            resource='connection',
+            resource_name='83c4a42b-31db-4655-9dc4-9576a0058bb7'
+        )
+
+        pipeline = CodePipeline(self, 'MyPipeline',
             pipeline_name="MyPipeline",
             synth=ShellStep("Synth", 
-                input=CodePipelineSource.git_hub('SuperBo/my-aws-app', 'main'),
+                input=CodePipelineSource.connection('SuperBo/my-aws-app', 'main',
+                    connection_arn=github_arn
+                ),
                 commands=[
                     'npm install -g aws-cdk', 
                     'python -m pip install -r requirements.txt', 
                     'cdk synth'
                 ]
-            )
+            ),
+            cross_account_keys=False,
         )
 
         pipeline.add_stage(MyPipelineAppStage(self, 'production'))
